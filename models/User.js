1e06ca263e0120ef9c5bc4b62e442153a928456f
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-27 21:47:44 +0100

    Setup User schema model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name!'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide a valid email address!'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a valid password!'],
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+});
+
+module.exports = mongoose.model('User', UserSchema);
