77dfe09925b2bf515e3909ddd7c898c7e9462614
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-27 21:30:29 +0100

    Setup basic routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 
 // Express
 const express = require('express');
@@ -6,6 +7,19 @@ const app = express();
 
 // database
 const connectDB = require('./db/connect');
+
+// middlewares
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('E-Commerce API');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
