2dcab553930bb68b1975c31ad0c810f3098d852a
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-28 20:03:01 +0100

    Use cookieParser middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 // rest of the packages
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 // database
 const connectDB = require('./db/connect');
@@ -20,6 +21,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get('/', (req, res) => {
     res.send('E-Commerce API');
