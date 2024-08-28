1bb30bc72c0b250af56d0db4fa04cc2b676855e3
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-28 21:19:27 +0100

    Add user routes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const connectDB = require('./db/connect');
 
 // routers
 const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // middlewares
 const notFoundMiddleware = require('./middleware/not-found');
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
