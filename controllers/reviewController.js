ffb3a8fd49e7d3c33554ac1bce4a996d4c3f9533
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-29 22:08:46 +0100

    Setup boilerplate code for review routes controllers

---
+++ controllers/reviewController.js
@@ -0,0 +1,32 @@
+const createReview = async (req, res) => {
+    res.send('Create Review');
+};
+
+const getAllReviews = async (req, res) => {
+    res.send('Get all reviews');
+};
+
+const getSingleReview = async (req, res) => {
+    res.send('Get single review');
+};
+
+const updateReview = async (req, res) => {
+    res.send('Update review');
+};
+
+const deleteReview = async (req, res) => {
+    res.send('Delete Review');
+};
+
+const getSingleProductReviews = async (req, res) => {
+    res.send('Get single Review');
+};
+
+module.exports = {
+    createReview,
+    getAllReviews,
+    getSingleReview,
+    updateReview,
+    deleteReview,
+    getSingleProductReviews,
+};
