51bfb1bbec3396796b9b460c0b3996d5be12376d
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-29 18:37:31 +0100

    Setup updateUserPassword route controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,7 +28,24 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-    res.send('Update user password');
+    const {oldPassword, newPassword} = req.body;
+
+    if (!oldPassword || !newPassword) {
+        throw new CustomError.BadRequestError('Please provide both values');
+    }
+
+    const user = await User.findOne({_id: req.user.userId});
+
+    const isPasswordCorrect = await user.comparePassword(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+    res.status(StatusCodes.OK).json({msg: 'Success! Password Updated.'});
 };
 
 module.exports = {
